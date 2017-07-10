0a4aaec3c5781b1e731193e3eb56f799d183001b
Mark <lamson_mark@yahoo.com>
2017-07-10 09:09:47 -0400

    create loop for placePieceInLowestOpenSpace in place of all else if statements

--- src/app/player-state.ts
+++ src/app/player-state.ts
@@ -39,28 +39,11 @@ export abstract class Player implements State {
   placePieceInLowestOpenSpace(column: number, row: number): void {
 
     if (this.gameboardComponent.gameboard[column][row] === "open") {
-
-    }
-
-    if (this.gameboardComponent.gameboard[column][row] === "open") {
-
-      if (this.gameboardComponent.gameboard[column][1] !== "open") {
-        this.gameboardComponent.gameboard[column][0] = this.gameboardComponent.state.currentPlayer;
-      }
-      else if (this.gameboardComponent.gameboard[column][2] !== "open") {
-        this.gameboardComponent.gameboard[column][1] = this.gameboardComponent.state.currentPlayer;
-      }
-      else if (this.gameboardComponent.gameboard[column][3] !== "open") {
-        this.gameboardComponent.gameboard[column][2] = this.gameboardComponent.state.currentPlayer;
-      }
-      else if (this.gameboardComponent.gameboard[column][4] !== "open") {
-        this.gameboardComponent.gameboard[column][3] = this.gameboardComponent.state.currentPlayer;
-      }
-      else if (this.gameboardComponent.gameboard[column][5] !== "open") {
-        this.gameboardComponent.gameboard[column][4] = this.gameboardComponent.state.currentPlayer;
-      }
-      else {
-        this.gameboardComponent.gameboard[column][5] = this.gameboardComponent.state.currentPlayer;
+      for (let index: number = 1; index < 7; index++) {
+        if (this.gameboardComponent.gameboard[column][index] !== "open") {
+          this.gameboardComponent.gameboard[column][index - 1] = this.gameboardComponent.state.currentPlayer;
+          return;
+        }
       }
     }
   }
